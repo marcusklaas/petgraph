87c7b7188746ce119bcee8228513d9df03951ebc
Marcus Klaas <mail@marcusklaas.nl>
2018-12-21 16:37:44 +0100

    play around a bit more with fas

--- src/fas.rs
+++ src/fas.rs
@@ -1,9 +1,22 @@
-use super::algo::is_cyclic_directed;
+//use super::algo::is_cyclic_directed;
 use super::visit::{EdgeRef, IntoEdges, IntoNeighbors, IntoNodeIdentifiers, Visitable};
 use std::collections::HashSet;
 use std::hash::Hash;
+use std::ops::Sub;
 
-fn find_cycle<G>(graph: G) -> Option<HashSet<G::EdgeRef>>
+pub trait UpdateWeight: EdgeRef {
+    fn set_weight(&mut self, new_weight: <Self as EdgeRef>::Weight);
+}
+
+// std::num::Zero is unstable
+// what to do here?
+// we could pass in a zero value explicitly
+// or we could compare to x - x with the assumption that it's 0
+pub trait HasZero {
+    fn zero() -> Self;
+}
+
+fn find_cycle<G>(graph: G, removed_edges: &HashSet<G::EdgeRef>) -> Option<Vec<G::EdgeRef>>
 where
     G: IntoNodeIdentifiers + IntoNeighbors + Visitable + IntoEdges,
 {
@@ -17,21 +30,37 @@ where
 pub fn fas<G>(graph: G) -> (G, HashSet<G::EdgeRef>)
 where
     G: IntoNodeIdentifiers + IntoNeighbors + Visitable + IntoEdges + Clone,
-    G::EdgeRef: Hash + Eq,
+    G::EdgeRef: Hash + Eq + UpdateWeight,
+    <G::EdgeRef as EdgeRef>::Weight: Ord + Sub<Output = <G::EdgeRef as EdgeRef>::Weight> + Clone + HasZero
 {
-    let cloned = graph.clone();
+    // FIXME: we could probably do without a clone of the entire graph lol
+    let mut cloned = graph.clone();
     let mut arc_set = HashSet::new();
 
-    while let Some(cycle) = find_cycle(cloned) {
-        let _min_weight_arc = cycle.iter().map(|_| 1 /* get edge weight */).min();
-        for _edge in cycle {
-            // cloned.edge.weight -= _min_weight_arc;
-            // if cloned.edge.weight == 0 {
-            //     arc_set.insert(cloned.edge);
-            //     // cloned.remove_arc(cloned.edge); ??
-            // }
+    while let Some(ref mut cycle) = find_cycle(cloned, &arc_set) {
+        // FIXME: we should be able to work with fewer clones here
+        // FIXME: can we do a min without option result? we know we always get a value here
+        if let Some(min_weight_arc) = cycle.iter().map(EdgeRef::weight).cloned().min() {
+            for edge in cycle.iter_mut() {
+                let old_weight = edge.weight().clone();
+                edge.set_weight(old_weight - min_weight_arc.clone());
+                if edge.weight() <= &<<G::EdgeRef as EdgeRef>::Weight as HasZero>::zero() {
+                    arc_set.insert(edge.clone()); // more clones lol
+                }
+            }
         }
     }
 
-    (cloned, arc_set)
+    let mut final_arc_set: HashSet<_> = arc_set.clone();
+
+    for edge in &arc_set {
+        final_arc_set.remove(edge);
+
+        if find_cycle(cloned, &final_arc_set).is_some() {
+            // adding this edge back introduces a cycle, so definitively remove it
+            final_arc_set.insert(*edge);
+        }
+    }
+
+    (cloned, final_arc_set)
 }
